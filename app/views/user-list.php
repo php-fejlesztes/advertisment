12e5ae644594c8d0f5e6a4537aec0b837fb5e5f0
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-11-14 10:49:33 +0100

    Módosítás lehetőség

--- app/views/user-list.php
+++ app/views/user-list.php
@@ -15,7 +15,20 @@
         </div>     
         <h1>Felhasználók kezelése</h1>
         <p>A következő listában azokat a felhasználókat látja, akik hirdetéseket adhatnak fel.</p>
-        <h2>Felhasználók listája:</h2>
+        <h2>Felhasználók adatai:</h2>
+        <?php 
+                echo '<p>Új felhasználó adatainak megadása:</p>';
+                echo '<form class="form" action="#">';
+                    echo '<p>Azon.</p>';
+                    echo '<p>1</p>';
+                    echo '<label class="form-label" for="name">Felhasználó név:</label>';
+                    echo '<input class="form-text" id="name" type="text"  name="" value="">';
+                    echo '<div class="submit-wrapper">';
+                        echo '<button type="submit"><img class="icon-button" src="/advertisment/public/img/trash.png"></img> Mentés</button>';
+                    echo '</div>'; //submitwrapper
+                echo '</form>';
+        ?>
+        <p>Felhasználók listája:</p>
         <?php       
             if (!isset($users) ||  $users==null) {
                     echo "<p>Nincs felhasználó az adatbázisban!</p>";
@@ -25,7 +38,8 @@
                 echo '<div class="row-head">';
                 echo '<div class="cell-id">Azon.</div>';
                 echo '<div class="cell-name">Felhasználó név</div>';
-                echo '</div>';
+                echo '<div class="cell-id">Műveletek</div>';
+                echo '</div>'; //row
                 $i=0;
                 foreach($users as $user) {   
                     if ($i%2==0)
@@ -34,12 +48,17 @@
                         echo '<div class="row-light">';
                     echo '<div class="cell-id">'.$user->getUserId().'</div>';
                     echo '<div class="cell-name">'.$user->getUserName().'</div>';
-                    echo '</div>';
+                    echo '<div class="cell-id">';
+                        echo '<a href="#"><img class="icon" src="/advertisment/public/img/pencil.png"></img></a>';
+                        echo '<a href="#"><img class="icon" src="/advertisment/public/img/trash.png"></img></a>';
+                    echo '</div>'; // műveletek
+                    echo '</div>'; // row
                     $i=$i+1;
                 }    
-                echo '</div>';
+                echo '</div>'; // table
             }
         ?>
+        <p>
     </div>
 </body>
 </html>
